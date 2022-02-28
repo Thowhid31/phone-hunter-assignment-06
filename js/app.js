7b547bf2396212f1bc7e87b7bfd22941f68be8e2
Thowhid31 <thowhidantor16@gmail.com>
2022-03-01 00:06:22 +0600

    added details part-1

--- js/app.js
+++ js/app.js
@@ -29,7 +29,6 @@ const displayPhones = (disPhones) => {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
-        // div.classList.add('col-sm-1');
         div.classList.add('mb-3');
         div.classList.add('container');
         div.innerHTML = `
@@ -38,11 +37,64 @@ const displayPhones = (disPhones) => {
         <div class="container">
             <h5 class="card-title mt-4">${phone.brand}</h5>
             <p class="card-text">${phone.phone_name}</p>
-            <a href="#" onclick="cardDetails('${phone.code}')" class="btn btn-primary mb-3">Details</a>
+            <button onclick="details('${phone.slug}')" class="btn btn-primary mb-3">Details</button>
         </div>
         </div>
         `
-        main.appendChild(div)
+        main.appendChild(div);
     }
+
 }
 
+
+
+
+        
+   
+
+    
+const details = (slug) => {
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    .then(res => res.json())
+    .then(data => {
+        const allPhones = data.data;
+        const singlePhone = allPhones.find(phone => phone.slug == slug);
+        const div = document.createElement('div');
+        main.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${singlePhone.image}" class="card-img-top" alt="...">
+        <div class="container">
+            <h5 class="card-title mt-4">${singlePhone.brand}</h5>
+            <p class="card-text">${singlePhone.phone_name}</p>
+        </div>
+        </div>
+        `
+        main.appendChild(div);
+
+        
+    }) 
+} 
+
+
+
+    // const displayDetail = (phones) => {
+    //     console.log('coooooool');
+
+        
+        // for (const detail of phones){
+        //     console.log(detail)
+        //     const div = document.createElement('div');
+        //     div.classList.add('container');
+        // div.innerHTML = `
+        // <div class="card" style="width: 18rem;">
+        // <img src="${detail.image}" class="card-img-top" alt="...">
+        // <div class="container">
+        //     <h5 class="card-title mt-4">${detail.brand}</h5>
+        //     <p class="card-text">${detail.phone_name}</p>
+        // </div>
+        // </div>
+        // `
+        // main.appendChild(div);
+        // }
+
+// }
