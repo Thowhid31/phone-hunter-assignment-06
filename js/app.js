1cede251c7e39aab17d051df83cac4a3d12d943f
Thowhid31 <thowhidantor16@gmail.com>
2022-03-01 22:52:46 +0600

    finished successfully

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
-
+// search part here
 const main = document.getElementById('mainid')
 const searchButton = () => {
     const inputBox = document.getElementById('input-value');
     const geterror = document.getElementById('error');
 
     const searchText = inputBox.value;
-    if(searchText == '' || searchText == undefined){
+    if(searchText == ''){
         geterror.innerText = 'Please give meaningful name';
         main.innerHTML = '';
     }
@@ -22,11 +22,20 @@ const searchButton = () => {
 
         document.getElementById('input-value').value = '';
         geterror.innerText = '';
+        const detailId = document.getElementById('detail-field');
+        detailId.innerHTML = '';
     }
 
 }
 
+//display starts here
+
 const displayPhones = (disPhones) => {
+    if(disPhones.length == 0){
+        const inputError = document.getElementById('error');
+        inputError.innerText = 'No phone Found!'
+    }
+
     const showDiv = document.getElementById('show-details');
     showDiv.innerHTML = '';
     for (const phone of disPhones){
@@ -56,7 +65,7 @@ const displayPhones = (disPhones) => {
 
 }
 
-    main.innerHTML = '';
+//show details starts here
 const loadPhoneDetail = phoneId => {
     // console.log(phoneId);
     
@@ -67,13 +76,15 @@ const loadPhoneDetail = phoneId => {
         .then(data => showDetail(data.data));
 }
 
+
 const showDetail = showPhoneDetail => {
-    console.log(showPhoneDetail)
+    // console.log(showPhoneDetail)
     const detailId = document.getElementById('detail-field');
+    detailId.innerHTML = '';
     const div = document.createElement('div');
     // div.classList.add('card');
     div.innerHTML = `
-    <div class="card bg-success bg-gradient bg-opacity-25 m-5 p-2 d-flex center" style="width: 18rem;">
+    <div class="card bg-success bg-gradient bg-opacity-25 m-5 p-2" style="width: 30rem;">
         <h1 class="text-success w-100 text-center"> Details your searched Phone</h1>
         <img src="${showPhoneDetail.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -81,22 +92,20 @@ const showDetail = showPhoneDetail => {
         <p class="card-text fw-bold">Release Date: ${showPhoneDetail.releaseDate ? showPhoneDetail.releaseDate:'release date not found'}</p>
         <p class="card-text fw-bold">Phone Name: ${showPhoneDetail.name}</p>
         <h5 class="text-danger"> Main Features </h5>
-        <p class="card-text fw-bold"> Storage: ${showPhoneDetail.mainFeatures.storage ? showPhoneDetail.storage:'Storage not found'}</p>
-        <p class="card-text fw-bold">Sensors: ${showPhoneDetail.mainFeatures.sensors ? showPhoneDetail.sensors:'N/A'}</p>
-        <p class="card-text fw-bold">Memory: ${showPhoneDetail.mainFeatures.memory ? showPhoneDetail.memory:'N/A'}</p>
-        <p class="card-text fw-bold">Others: ${showPhoneDetail.others.WLAN ? showPhoneDetail.WLAN:'N/A'}</p>
-        <p class="card-text fw-bold">Bluetooth${showPhoneDetail.others.Bluetooth ? showPhoneDetail.Bluetooth:'N/A'}</p>
-        <p class="card-text fw-bold">GPS: ${showPhoneDetail.others.GPS ? showPhoneDetail.GPS:'N/A'}</p>
-        <p class="card-text fw-bold">NFC: ${showPhoneDetail.others.NFC ? showPhoneDetail.NFC:'N/A'}</p>
-        <p class="card-text fw-bold">Radio: ${showPhoneDetail.others.Radio ? showPhoneDetail.Radio:'N/A'}</p>
-        <p class="card-text fw-bold">USB: ${showPhoneDetail.others.USB ? showPhoneDetail.USB:'N/A'}</p>
+        <p class="card-text fw-bold"> Storage: ${showPhoneDetail.mainFeatures.storage ? showPhoneDetail.mainFeatures.storage:'Storage not found'}</p>
+        <p class="card-text fw-bold">Sensors: ${showPhoneDetail.mainFeatures.sensors ? showPhoneDetail.mainFeatures.sensors:'N/A'}</p>
+        <p class="card-text fw-bold">Memory: ${showPhoneDetail.mainFeatures.memory ? showPhoneDetail.mainFeatures.memory:'N/A'}</p>
+        <p class="card-text fw-bold">Others: ${showPhoneDetail?.others?.WLAN ? showPhoneDetail.others.WLAN:'N/A'}</p>
+        <p class="card-text fw-bold">Bluetooth${showPhoneDetail?.others?.Bluetooth ? showPhoneDetail.others.Bluetooth:'N/A'}</p>
+        <p class="card-text fw-bold">GPS: ${showPhoneDetail?.others?.GPS ? showPhoneDetail.others.GPS:'N/A'}</p>
+        <p class="card-text fw-bold">NFC: ${showPhoneDetail?.others?.NFC ? showPhoneDetail.others.NFC:'N/A'}</p>
+        <p class="card-text fw-bold">Radio: ${showPhoneDetail?.others?.Radio ? showPhoneDetail.others.Radio:'N/A'}</p>
+        <p class="card-text fw-bold">USB: ${showPhoneDetail?.others?.USB ? showPhoneDetail.others.USB:'N/A'}</p>
     </div>
     <div>
     
     </h1>
     `
-
     detailId.appendChild(div);
-    
-    
+    // main.innerHTML = '';
 }
